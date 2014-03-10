bfa95e0765e4cf3d76356a03465a6f5994e995f5
iwebsource <iwebsource@eastlink.ca>
2014-03-10 16:03:19 -0300

    Initial Upload of Foundation Rails Project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Found::Application.config.secret_key_base = 'f2fa0f7b270c3d05bfadc8d7da841bbb02e7a4ee4908eec43b98b966ba968b60a8bb1b3d5373846fba0f37dc55ad2a9ed6609303e70aa71486704f3e94a86c4d'
