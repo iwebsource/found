878bd8a5d97a52cba43fcc11e3ae9eca30283965
iwebsource <iwebsource@eastlink.ca>
2014-03-15 02:02:21 -0300

    Minor tonight changes 1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  def create
+  @user = User.create( user_params )
+end
+
+private
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 
