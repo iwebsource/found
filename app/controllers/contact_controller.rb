daad49bd3d2645f315d570afa1ba3ef96c01aa63
iwebsource <iwebsource@eastlink.ca>
2014-03-15 14:09:57 -0300

    Fixed Notification Mailer

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -6,7 +6,7 @@ end
 def create
 @message = Message.new(params[:message])
 if @message.valid?
-NotificationsMailer.new_message(@message).deliver
+NotificationMailer.new_message(@message).deliver
 redirect_to(root_path, :notice => "Message was successfully sent.")
 else
 flash.now.alert = "Please fill all fields."
