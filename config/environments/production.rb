2f82c99a9f5dac0ded46eeec863739eeeb6b51c4
iwebsource <iwebsource@eastlink.ca>
2014-03-17 12:58:21 -0300

    Added Sendmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,15 +35,19 @@ Found::Application.configure do
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => 'rails.iwebdev.ca',
-  :user_name            => 'iwebsource1@gmail.com',
-  :password             => 'iwebAdmin3266',
-  :authentication       => 'plain',
-  :enable_starttls_auto => true  }
+  # Mail Settings
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  #config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.smtp_settings = {
+  #:address              => "smtp.gmail.com",
+  #:port                 => 587,
+  #:domain               => 'rails.iwebdev.ca',
+  #:user_name            => 'iwebsource1@gmail.com',
+  #:password             => '',
+  #:authentication       => 'plain',
+  #:enable_starttls_auto => true  }
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
