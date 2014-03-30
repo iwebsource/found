78c26cb7aee41682e1921d84702182f346b452ad
iwebsource <iwebsource@eastlink.ca>
2014-03-30 01:41:13 -0300

    Fixed Generic Signup Email Reg. Controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,10 +5,34 @@
    def configure_permitted_parameters
      devise_parameter_sanitizer.for(:sign_in){ |u| u.permit(:email, :password) }
      devise_parameter_sanitizer.for(:sign_up){ |u| u.permit(:full_name, :email, :password, :password_confirmation)}
-     devise_parameter_sanitizer.for(:account_update){ |u| u.permit(:full_name, :email, :password, :password_confirmation) }
+     devise_parameter_sanitizer.for(:account_update){ |u| u.permit(:full_name, :email, :password, :password_confirmation, :current_password) }
    end
 
-   def update
+  def create
+    build_resource(sign_up_params)
+
+    resource_saved = resource.save
+    yield resource if block_given?
+    if resource_saved
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_flashing_format?
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+        WelcomeMailer.registration_confirmation(@user).deliver
+      else
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      respond_with resource
+    end
+  end
+
+
+
+ def update
      self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
      if resource.update_with_password(user_params)
 
@@ -23,6 +47,6 @@
    end
 
    def user_params
-     params.require(:user).permit(:full_name, :email, :password, :current_password, :password_confirmation)
+     params.require(:user).permit(:full_name, :email, :password, :password_confirmation, :current_password)
    end
  end
